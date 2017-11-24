a20a089991ef728abbf596e01b0bd4b919e26418
Your Name <you@example.com>
2017-11-24 21:03:38 +0100

    advanced error checks on user input

--- tst3/app.py
+++ tst3/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request
-import datetime
+from datetime import datetime
+import time
 import yaml
 import os.path
 from threading import Thread, Lock
@@ -21,7 +22,7 @@ class GLOBAL_DATA:
             self.mutex.release()
 
     def _convertToTime(self, aString):
-        return datetime.datetime.strptime(aString, '%H:%M').time()
+        return datetime.strptime(aString, '%H:%M').time()
 
     def _getYamlFileName(self):
         return 'data.yml'
@@ -33,12 +34,14 @@ class GLOBAL_DATA:
             #    yaml.dump(data, outfile, default_flow_style=False)
             if active == "true":
                 try:
-                    self._convertToTime(start)
+                    _start = self._convertToTime(start)
                     print("start time is valid")
-                    self._convertToTime(end)
+                    _end = self._convertToTime(end)
                     print("end time is valid")
+                    if _end <= _start:
+                        return("start cannot be after end")
 
-                    uidStr = "UID" + uid
+                    uidStr = "UID" + str(uid)
                     self.yamlData[uidStr]["active"] = active
                     self.yamlData[uidStr]["startTime"] = start
                     self.yamlData[uidStr]["endTime"] = end
@@ -47,17 +50,28 @@ class GLOBAL_DATA:
                     with open(fName, "w") as f:
                         yaml.dump(self.yamlData, f)
                 except ValueError:
-                    print("could not convert some timestamp")
+                    return("Error on time conversion")
         finally:
             self.mutex.release()
-
+        return ""
 
 
     def yaml_isActive(self, uid):
         self.mutex.acquire()
         try:
-            uidStr = "UID" + uid
+            uidStr = "UID" + str(uid)
             return self.yamlData[uidStr]["active"] == "true"
+        finally:
+            self.mutex.release()
+
+    def yaml_info_get(self, uid):
+        self.mutex.acquire()
+        try:
+            uidStr = "UID" + str(uid)
+            active = self.yamlData[uidStr]["active"] == "true"
+            startTime = self._convertToTime(self.yamlData[uidStr]["startTime"])
+            endTime = self._convertToTime(self.yamlData[uidStr]["endTime"])
+            return [active, startTime, endTime]
         finally:
             self.mutex.release()
 
@@ -68,14 +82,14 @@ class GLOBAL_DATA:
                 UID0 = dict(
                     UID = '0',
                     active = 'false',
-                    startTime = '00:0:00',
-                    endTime = '00:0:00')
+                    startTime = '00:00',
+                    endTime = '00:00')
                 ,
                 UID1 = dict(
                     UID = '1',
                     active = 'false',
-                    startTime = '00:0:00',
-                    endTime = '00:0:00')
+                    startTime = '00:00',
+                    endTime = '00:00')
                 )
         with open(fName, "w") as f:
             yaml.dump(self.yamlData, f)
@@ -98,13 +112,14 @@ _GDATA = GLOBAL_DATA()
 
 
 @app.route("/")
-def index_html():
+def index_html(errorString  = ''):
     global _GDATA
     print("index html")
-    now = datetime.datetime.now()
+    now = datetime.now()
     timeString = now.strftime("%Y-%m-%d %H:%M")
     yamlData = _GDATA.yaml_data_get()
     templateData = {
+        'error_text' : errorString,
         'title' : 'HELLO!',
         'time': timeString,
         'uid0_active': yamlData["UID0"]["active"],
@@ -118,7 +133,7 @@ def index_html():
 
 @app.route("/timepicker.html", methods=['GET'])
 def timepicker():
-    now = datetime.datetime.now()
+    now = datetime.now()
     timeString = now.strftime("%Y-%m-%d %H:%M")
     uid = request.args.get('UID')
     if uid == None:
@@ -139,11 +154,31 @@ def timepicker_report():
     active = request.form.get('active')
     UID = request.form.get('UID')
     print(f"timepicker_report {UID} {active} {startTime} {endTime} ")
-    _GDATA.updateConfigFile(uid = UID,active = active, start = startTime, end = endTime)
-    return index_html()
+    errorText = _GDATA.updateConfigFile(uid = UID,active = active, start = startTime, end = endTime)
+    return index_html(errorText)
 
 
+class myThread (Thread):
+    def __init__(self, threadID, name):
+        Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.PinActive = False
+    
+    def processUid(self, uid):
+        [active, startTime, endTime] = _GDATA.yaml_info_get(uid)
+        #if active and 
 
+    def run(self):
+        global _GDATA
+        print ("Starting " + self.name)
+        while(1):
+            time.sleep(1)
+            for uid in range(1):
+                self.processUid(uid)
+        print ("Exiting " + self.name)
 
 if __name__ == "__main__":
+    thread1 = myThread(1, "PinWorker")
+    thread1.start()
     app.run(host='0.0.0.0', port=5000, debug=True)
