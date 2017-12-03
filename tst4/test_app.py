b99a4136ced4fef646c50a57a717ac730d3de3d4
Your Name <you@example.com>
2017-12-03 09:55:02 +0100

    tests++

--- tst4/test_app.py
+++ tst4/test_app.py
@@ -45,18 +45,38 @@ class FlaskrTestCase(unittest.TestCase):
         assert b'UID0_active false' in rv.data
         assert b'UID1_active false' in rv.data
         assert b'CurrentStatus off' in rv.data
+
+    def _assert_uid_timer(self, uid0_start_stop, asciihtml):
+        uid = uid0_start_stop[0]
+        startTime = uid0_start_stop[1]
+        stopTime = uid0_start_stop[2]
+        assert 'UID' + str(uid) + '_active true' in asciihtml
+        assert 'UID' + str(uid) + '_start ' + startTime in asciihtml
+        assert 'UID' + str(uid) + '_stop ' + stopTime in asciihtml
+
+
+    def _set_and_verify_timers(self,uid0_start_stop, uid1_start_stop):
+        self._set_and_verify_timer(uid0_start_stop[0], uid0_start_stop[1], uid0_start_stop[2])
+        asciihtml = self._set_and_verify_timer(uid1_start_stop[0], uid1_start_stop[1], uid1_start_stop[2])
+        asciihtml = self._set_and_verify_timer(uid1_start_stop[0], uid1_start_stop[1], uid1_start_stop[2])
+        self._assert_uid_timer(uid0_start_stop, asciihtml)
+        self._assert_uid_timer(uid1_start_stop, asciihtml)
+
+    def _set_and_verify_timer(self,uid,startTime, stopTime):
+        print("_set_and_verify_timer " + str(uid) + " " + startTime + " " + stopTime)
+        rv = self._setTimer(uid,startTime, stopTime)
+        rvascii = rv.data.decode("ascii")
+        self._assert_uid_timer((uid,startTime,stopTime),rvascii)
         
-    #with freeze_time("2012-01-14 23:00:00", tz_offset=2):
-    def test_set01(self):
+    def test_single_sets_01(self):
         with freeze_time("2017-01-01 00:01"):
-            uid = 0
-            startTime = "00:30"
-            stopTime  = "01:00"
-            rv = self._setTimer(uid,startTime, stopTime)
-            rvascii = rv.data.decode("ascii")
-            assert 'UID' + str(uid) + '_active true' in rvascii
-            assert 'UID' + str(uid) + '_start ' + startTime in rvascii
-            assert 'UID' + str(uid) + '_stop ' + stopTime in rvascii
+            testItems = ((0,"00:30", "01:00"),
+                         (0,"22:31", "23:01"),
+                         (1,"00:31", "01:01"),
+                         (1,"23:31", "23:41")
+                        )
+            for item in testItems:
+                self._set_and_verify_timer(item[0],item[1], item[2])
 
 
 if __name__ == '__main__':
